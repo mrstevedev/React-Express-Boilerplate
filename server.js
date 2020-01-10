93cb942bf831053dc824a01e6f35974608eecced
Steven Polido <steven.polido@vistage.com>
2020-01-09 20:30:26 -0800

    Updated folder.

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ if(process.env.NODE_ENV === 'production'){
     // Set static folder 
     app.use(express.static('client_src/dist'));
 
+    
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client_src', 'dist', 'index.html'));
     });
