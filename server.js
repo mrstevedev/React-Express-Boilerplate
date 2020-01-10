c52f136ab350d43bf59d12d2c8b1caf298d2b6ff
Steven Polido <steven.polido@vistage.com>
2020-01-09 19:58:53 -0800

    Set static files to be served if in production.

--- server.js
+++ server.js
@@ -2,18 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/dist')));
-
 app.get('/api/getRecords', (req, res) => {
     const list = ['The Nobodies ‎– Its Hip Hop / What You Hear', 'MC Gels ‎– Wandering Souls', 'Overtone ‎– All The Things We Are'];
     res.json(list);
     console.log('Sent a list of vinyl records');
 });
-
-const PORT = process.env.PORT || 5002;
-
-app.listen(PORT, () => console.log(`Server starter on port ${PORT}`));
 
 // Routes
 app.get('/', (req, res) => {
@@ -24,3 +17,17 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/dist/index.html'));
 });
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder 
+    app.use(express.static('client_src/dist'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client_src', 'dist', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5002;
+
+app.listen(PORT, () => console.log(`Server starter on port ${PORT}`));
